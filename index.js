1fce86b22c94d8f47d8f3fa07e068b7b28de0619
Clay Mangum <clay@trinitasaccounting.com>
2024-01-30 17:31:15 -0600

    complete

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+
+let totalBatteries = 0;
+const reducer = batteryBatches.reduce((acc, val) => {
+    totalBatteries = val + totalBatteries;
+    return totalBatteries;
+}, 0)
+
+console.log(reducer);
